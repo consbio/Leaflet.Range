b699a726f7c5079467a881c4306dc0ffafdfa2d5
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2025-09-16 19:15:07 -0700

    Update gulp dependency, use pnpm

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@ var gulp = require('gulp');
 var minify = require('gulp-minify');
 
 
-gulp.task('build', function () {
-    gulp.src('L.Control.Range.js')
+function build() {
+    return gulp.src('L.Control.Range.js')
         .pipe(minify({noSource: true}))
         .pipe(gulp.dest('./'))
-});
+}
 
-gulp.task('watch', function () {
-    gulp.watch('L.Control.Range.js', ['build']);
-});
+function watch() {
+    return gulp.watch('L.Control.Range.js', ['build']);
+}
 
-gulp.task('default', ['build', 'watch']);
+exports.build = build
+exports.watch = watch
+exports.default = build
